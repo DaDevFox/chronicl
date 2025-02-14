8b0968988ed7a45492c3d6699c4df634d977ca64
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2025-02-14 00:21:48 -0500

    FIX(CFG): read config of any form chronicl.(yaml|toml|other)

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,10 @@
 package config
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type CommitType struct {
@@ -22,21 +24,34 @@ type Config struct {
 // LoadConfig tries to read config from multiple sources
 func LoadConfig() (*Config, error) {
 	// var config Config
+	dir, _ := os.Getwd()
 	paths := []string{
-		filepath.Join(os.Getenv("HOME"), ".chroniclrc"), // YAML default
-		filepath.Join(os.Getenv("HOME"), ".chroniclrc.toml"),
-		// filepath.Join(os.Getenv("HOME"), ".rgrc.textproto"),
+		os.Getenv("HOME"),
+		dir,
 	}
 
 	for _, path := range paths {
-		if stat, err := os.Stat(path); err == nil && stat.Name() == "chronicl" {
-			switch filepath.Ext(path) {
-			case ".yaml", ".yml", "":
-				return LoadYAML(path)
-			case ".toml":
-				return LoadTOML(path)
-				// case ".textproto":
-				// 	return LoadTextProto(path)
+		f, err := os.Open(path)
+		if err != nil {
+			fmt.Printf("%v", err)
+		}
+		defer f.Close()
+
+		files, err := f.Readdir(-1)
+		if err != nil {
+			fmt.Printf("%v", err)
+		}
+
+		for _, file := range files {
+			if strings.HasPrefix(file.Name(), "chronicl") {
+				switch filepath.Ext(file.Name()) {
+				case ".yaml", ".yml", "":
+					return LoadYAML(filepath.Join(path, file.Name()))
+				case ".toml":
+					return LoadTOML(filepath.Join(path, file.Name()))
+					// case ".textproto":
+					// 	return LoadTextProto(path)
+				}
 			}
 		}
 	}
